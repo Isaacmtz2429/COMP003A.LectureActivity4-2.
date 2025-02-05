c9b18185cae6e92574b2f94c0d8ac05dfe899b3b
Isaac Martinez <isaacmartinez2429@gmail.com>
2025-02-05 13:51:34 -0800

    Add project files.

---
+++ COMP003A.LectureActivity4-2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+// Author: Isaac Martinez
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Practice implementing methods in C#
+*/
+namespace COMP003A.LectureActivity4_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
